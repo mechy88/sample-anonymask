e1d4a635b610c2dabc34ea01fbc2713f02025a88
mechy88 <miguelandrerepatacodo@gmail.com>
2025-07-04 10:09:44 +0800

    ADD: Password Digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_04_013450) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_04_020541) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
     t.integer "user_id"
@@ -42,5 +42,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_04_013450) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
