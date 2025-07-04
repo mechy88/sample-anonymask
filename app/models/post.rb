3e073c78353b508b5445ae171f1986769518cc65
mechy88 <miguelandrerepatacodo@gmail.com>
2025-07-04 09:25:15 +0800

    ADD: post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  enum :status, { unseen: 0, seen: 1, resolved: 2 }
+
+  validates :user_id, presence: true
+
+  validates :title, presence: true, length: { maximum: 300 }
+  validates :content, presence: true, length: { maximum: 40000 }
+  validates :status, presence: true, inclusion: { in: statuses.keys, message: "Post does not have a valid status" }
 end
