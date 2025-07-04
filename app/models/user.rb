c8f0a08f80c5cedec08ce0db61a745bce5781135
mechy88 <miguelandrerepatacodo@gmail.com>
2025-07-04 09:04:29 +0800

    ADD: User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 320 }, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } # TODO: verify if this really is an email, with the @ and everything
+  validates :role, presence: true, inclusion: { in: [ 0, 1 ], message: "You do not have a valid role" }
 end
